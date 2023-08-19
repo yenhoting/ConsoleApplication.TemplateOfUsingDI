9218ee29c07adc418b54c45745d05c3a653f93f2
Justin Yen <justinyen@systexsoftware.com.tw>
2023-08-19 17:50:46 +0800

    加入專案檔案。

---
+++ App.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApplication.TemplateOfUsingDI
+{
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+        private readonly IConfiguration _config;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public App(ILogger<App> logger, IConfiguration config, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _config = config;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task Run()
+        {
+            await Task.WhenAll(new List<Task> { FirstAction(), SecondAction() });
+        }
+
+        public async Task FirstAction()
+        {
+            await Task.Run(() => { });
+        }
+
+        public async Task SecondAction()
+        {
+            await Task.Run(() => { });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApplication.TemplateOfUsingDI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = new HostBuilder()
+             .ConfigureAppConfiguration((hostContext, config) =>
+             {
+                 config.AddJsonFile("appsettings.json", optional: true);
+                 config.AddEnvironmentVariables();
+             })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHttpClient();
+                services.AddTransient<App>();
+            });
+
+builder.Build().Services.GetRequiredService<App>().Run().Wait();
+
